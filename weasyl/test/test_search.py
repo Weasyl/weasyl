8fa2e70e8eda6421dfbf512254da14ba8b5b66d1
Charmander <~@charmander.me>
2016-06-14 00:30:10 -0700

    Test weasyl.search.Query.__nonzero__

--- weasyl/test/test_search.py
+++ weasyl/test/test_search.py
@@ -6,28 +6,35 @@ from weasyl.test import db_utils
 from weasyl import search
 
 
-def do_search(query, find):
-    terms = {
-        'cat': None, 'subcat': None, 'within': '', 'rated': [],
-        'q': query, 'find': find, 'backid': None, 'nextid': None
-    }
-    return search.select(None, ratings.GENERAL.code, 100, **terms)
+def test_query_parsing():
+    assert not search.Query.parse('', 'submit')
+    assert not search.Query.parse('#character', 'submit')
+    assert search.Query.parse('tag_that_does_not_exist', 'submit')
+
+    query = search.Query.parse('one, two +three & -four |five |six user:a +user:b -user:c #general #moderate #submission', 'journal')
+
+    assert query.possible_includes == {'five', 'six'}
+    assert query.required_includes == {'one', 'two', 'three'}
+    assert query.required_excludes == {'four'}
+    assert query.required_user_includes == {'a', 'b'}
+    assert query.required_user_excludes == {'c'}
+    assert query.ratings == {ratings.GENERAL.code, ratings.MODERATE.code}
+    assert query.find == 'submit'
 
 
-@pytest.mark.parametrize(['term', 'category', 'n_results'], [
-    (u"shouldmatchnothing", "submit", 0),
-    (u"shouldmatchnothing\\k", "user", 0),
-    (u"nobodyatall", "user", 0),
-    (u"JasonAG", "user", 1),
-    (u"Jason", "user", 1),
-    (u"rob", "user", 1),
-    (u"twisted", "user", 2),
-    (u"sam", "user", 2),
-    (u"jason otherkin", "user", 2),
-    (u"ryan wildlife calvin", "user", 3),
-    (u"Marth", "user", 1),
+@pytest.mark.parametrize(['term', 'n_results'], [
+    (u"shouldmatchnothing\\k", 0),
+    (u"nobodyatall", 0),
+    (u"JasonAG", 1),
+    (u"Jason", 1),
+    (u"rob", 1),
+    (u"twisted", 2),
+    (u"sam", 2),
+    (u"jason otherkin", 2),
+    (u"ryan wildlife calvin", 3),
+    (u"Marth", 1),
 ])
-def test_user_search(db, term, category, n_results):
+def test_user_search(db, term, n_results):
     config = CharSettings({'use-only-tag-blacklist'}, {}, {})
     db_utils.create_user("Sam Peacock", username="sammy", config=config)
     db_utils.create_user("LionCub", username="spammer2800", config=config)
@@ -39,7 +46,7 @@ def test_user_search(db, term, category, n_results):
     db_utils.create_user("Twisted Mindset", username="twistedm", config=config)
     db_utils.create_user("Martha", config=config)
 
-    results, _, _ = do_search(term, category)
+    results = search.select_users(term)
     assert len(results) == n_results
 
 
@@ -50,5 +57,5 @@ def test_user_search_ordering(db):
     db_utils.create_user("user_Ab", username="userab", config=config)
     db_utils.create_user("user_Bb", username="userbb", config=config)
 
-    results, _, _ = do_search(u"user", "user")
+    results = search.select_users(u"user")
     assert [user["title"] for user in results] == ["user_aa", "user_Ab", "user_ba", "user_Bb"]

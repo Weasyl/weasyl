6ac600891d08c7fd3ea25c0bb8023755ffa2c32e
Charmander <~@charmander.me>
2016-06-17 20:20:50 -0700

    Index favorites by unixtime

--- libweasyl/libweasyl/models/tables.py
+++ libweasyl/libweasyl/models/tables.py
@@ -218,6 +218,7 @@ favorite = Table(
 
 Index('ind_favorite_userid', favorite.c.userid)
 Index('ind_favorite_type_targetid', favorite.c.type, favorite.c.targetid, unique=False)
+Index('ind_favorite_unixtime', favorite.c.unixtime)
 
 
 folder = Table(

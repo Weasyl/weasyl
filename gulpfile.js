83ff5533ab6a16b8948190523db3dce25c606cfb
weykent <weykent@weasyl.com>
2016-06-19 15:19:18 -0700

    Switch back to gulp-sass.
    ruby-sass no longer necessary. I guess it was a permissions problem,
    because it all builds without issue now.

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,13 @@ var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 var rename = require('gulp-rename');
 var rev = require('gulp-rev');
-var sass = require('gulp-ruby-sass');
+var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-    return sass('assets/scss/site.scss', {style: 'compressed'})
+    return gulp.src('assets/scss/site.scss')
+        .pipe(
+            sass({outputStyle: 'compressed'})
+                .on('error', sass.logError))
         .pipe(
             autoprefixer({
                 browsers: ['last 2 versions', 'Android >= 4.4'],

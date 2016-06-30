b65e6333afc7b3ee5ebff41213b00703dc7950ea
kfkitsune <GitHub.com@KyraVixen.com>
2016-06-30 17:20:43 -0400

    Update schema in libweasyl/models/tables/py; companion commit to 1ee2853

--- libweasyl/libweasyl/models/tables.py
+++ libweasyl/libweasyl/models/tables.py
@@ -472,7 +472,6 @@ profile = Table(
         'h': 'hide-profile-from-guests',
         'i': 'hide-profile-stats',
         'k': 'disallow-others-tag-removal',
-        'r': 'disallow-others-tag-editing',
 
         's': 'watch-user-submissions',
         'c': 'watch-user-collections',

64faeb3cb72ba81e7cad754d581d313439f62298
Charmander <~@charmander.me>
2016-08-25 18:17:30 -0700

    Use make_cover_image from libweasyl.images

--- weasyl/image.py
+++ weasyl/image.py
@@ -1,7 +1,6 @@
 # favorite.py
 
 import logging
-import os
 
 from sanpera.exception import SanperaError
 from sanpera.image import Image
@@ -10,7 +9,6 @@ import web
 
 from libweasyl import images
 
-from weasyl import files
 from weasyl.error import WeasylError
 
 
@@ -59,67 +57,11 @@ def check_type(filename):
         return im.original_format in ['JPEG', 'PNG', 'GIF']
 
 
-def _resize(im, width, height):
-    """
-    Resizes the image to fit within the specified height and width; aspect ratio
-    is preserved. Images always preserve animation and might even result in a
-    better-optimized animated gif.
-    """
-    # resize only if we need to; return None if we don't
-    if im.size.width > width or im.size.height > height:
-        im = im.resized(im.size.fit_inside((width, height)))
-        return im
-
-
-def resize(filename, width, height, destination=None, animate=False):
-    in_place = False
-    if not destination:
-        destination = filename + '.new'
-        in_place = True
-
-    im = read(filename)
-    if not images.image_extension(im):
-        raise WeasylError("FileType")
-
-    files.ensure_file_directory(filename)
-    im = correct_image_and_call(_resize, im, width, height)
-    if im is not None:
-        im.write(destination)
-        if in_place:
-            os.rename(destination, filename)
-
-    # if there's no need to resize, in-place resize is a no-op. otherwise copy
-    # the source to the destination.
-    elif not in_place:
-        files.copy(filename, destination)
-
-
 def make_cover(filename, destination=None):
     """
     Create a cover image file; if `destination` is passed, a new file will be
     created and the original left unaltered, else the original file will be
     altered.
     """
-    resize(filename, *images.COVER_SIZE, destination=destination)
-
-
-def correct_image_and_call(f, im, *a, **kw):
-    """
-    Call a function, passing in an image where the canvas size of each frame is
-    the same.
-
-    The function can return an image to post-process or None.
-    """
-
-    animated = len(im) > 1
-    # either of these operations make the image satisfy the contraint
-    # `all(im.size == frame.size for frame in im)`
-    if animated:
-        im = im.coalesced()
-    else:
-        im = im.cropped(im[0].canvas)
-    # returns a new image to post-process or None
-    im = f(im, *a, **kw)
-    if animated and im is not None:
-        im = im.optimized_for_animated_gif()
-    return im
+    im = images.make_cover_image(images.read(filename))
+    im.write(destination or filename)

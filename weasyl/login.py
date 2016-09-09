Weasyl/weasyl

commit 191233624ac52a57b924693ebd93279ec97096f0

Remove “impersonate user” feature
Now nothing is stored in additional_data. SFW mode
probably should be, though.

weasyl/login.py
@@ -33,11 +33,6 @@ def signout(request):
     sess = request.weasyl_session
     # unset SFW-mode cookie on logout
     request.delete_cookie_on_response("sfwmode")
-    if sess.additional_data.get('user-stack'):
-        sess.userid = sess.additional_data['user-stack'].pop()
-        sess.additional_data.changed()
-    else:
-        sess.userid = None
     sess.save = True
 
 

templates/admincontrol/manageuser.html
@@ -1,4 +1,4 @@
-$def with (profile, can_impersonate)
+$def with (profile)
 $:{TITLE("User Management", "Administrator Control Panel", "/admincontrol")}
 
 <div class="content">
@@ -56,10 +56,5 @@ $:{TITLE("User Management", "Administrator Control Panel", "/admincontrol")}
     <button type="submit" class="button highlighted" style="float: right;">Save Changes</button>
     <a class="button" href="/admincontrol">Go Back</a>
     <a class="button" href="/~${LOGIN(profile['username'])}">Profile</a>
-    $if can_impersonate:
-      <button type="submit" class="button" style="float: right; clear: both;"
-              name="impersonate" value="yes">
-        Impersonate user
-      </button>
   </form>
 </div>

weasyl/controllers/admin.py
@@ -47,8 +47,6 @@ def admincontrol_manageuser_get_(request):
     return Response(d.webpage(request.userid, "admincontrol/manageuser.html", [
         # Manage user information
         profile.select_manage(otherid),
-        # only technical staff can impersonate users
-        request.userid in staff.TECHNICAL,
     ]))
 
 
@@ -61,27 +59,16 @@ def admincontrol_manageuser_post_(request):
 
     if request.userid != userid and userid in staff.ADMINS and request.userid not in staff.TECHNICAL:
         return d.errorpage(request.userid, errorcode.permission)
-    if form.get('impersonate'):
-        if request.userid not in staff.TECHNICAL:
-            return d.errorpage(request.userid, errorcode.permission)
-        sess = request.weasyl_session
-        sess.additional_data.setdefault('user-stack', []).append(sess.userid)
-        sess.additional_data.changed()
-        sess.userid = userid
-        sess.save = True
-        d.append_to_log(
-            'staff.actions', userid=request.userid, action='impersonate', target=userid)
-        raise HTTPSeeOther(location="/")
-    else:
-        profile.do_manage(request.userid, userid,
-                          username=form.username.strip() if form.ch_username else None,
-                          full_name=form.full_name.strip() if form.ch_full_name else None,
-                          catchphrase=form.catchphrase.strip() if form.ch_catchphrase else None,
-                          birthday=form.birthday if form.ch_birthday else None,
-                          gender=form.gender if form.ch_gender else None,
-                          country=form.country if form.ch_country else None,
-                          remove_social=form.remove_social)
-        raise HTTPSeeOther(location="/admincontrol")
+
+    profile.do_manage(request.userid, userid,
+                      username=form.username.strip() if form.ch_username else None,
+                      full_name=form.full_name.strip() if form.ch_full_name else None,
+                      catchphrase=form.catchphrase.strip() if form.ch_catchphrase else None,
+                      birthday=form.birthday if form.ch_birthday else None,
+                      gender=form.gender if form.ch_gender else None,
+                      country=form.country if form.ch_country else None,
+                      remove_social=form.remove_social)
+    raise HTTPSeeOther(location="/admincontrol")
 
 
 @token_checked

commit b2d8d25f8f2905653757a5f23a228e03302ba047

Fix logout
Overzealous deletion from 191233624ac52a57b924693ebd93279ec97096f0
caused by `git add -p` abuse.

weasyl/login.py
@@ -33,6 +33,7 @@ def signout(request):
     sess = request.weasyl_session
     # unset SFW-mode cookie on logout
     request.delete_cookie_on_response("sfwmode")
+    sess.userid = None
     sess.save = True
 
 
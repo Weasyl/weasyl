f30b5d30aef4eb79bcb5498355ba70e15b60c082
weykent <weykent@weasyl.com>
2016-12-20 19:51:15 -0800

    Postgres has changed a bit.
    The database dump is a .xz now and the 9.5 base image apparently runs
    the initdb.d files differently.

--- docker/db-import.sh
+++ docker/db-import.sh
@@ -1,8 +1,7 @@
-#!/bin/sh -eux
-dropdb "-U${POSTGRES_USER}" weasyl || true
+#!/bin/sh
 createdb "-U${POSTGRES_USER}" -E UTF8 weasyl
 createdb "-U${POSTGRES_USER}" -E UTF8 weasyl_test
 psql "-U${POSTGRES_USER}" weasyl -c 'CREATE EXTENSION hstore;'
-curl https://deploy.weasyldev.com/weasyl-latest-staff.sql.gz \
-    | gunzip \
+curl https://deploy.weasyldev.com/weasyl-latest-staff.sql.xz \
+    | xz -dc \
     | psql "-U${POSTGRES_USER}" weasyl

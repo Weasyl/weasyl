Weasyl/weasyl

commit c6455b4cd8c76203344d0403a1a13dca694c07c2

Add comments to site updates (#465)
This is not very nice. Sorry.

weasyl/comment.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 import arrow
 
 from libweasyl import staff
+from libweasyl.legacy import UNIXTIME_OFFSET
 
 from weasyl import define as d
 from weasyl import ignoreuser
@@ -20,15 +21,17 @@ def thread(query, reverse_top_level):
     Child comments are always in chronological order, but `reverse_top_level`
     controls the order of top-level comments.
     """
-    by_parent = {None: []}
+    by_parent = {None: (-1, [])}
 
     for row in query:
         parentid = row[1]
-        siblings = by_parent.get(parentid)
+        t = by_parent.get(parentid)
 
-        if siblings is None:
+        if t is None:
             # the parent comment isn’t visible to this user
             continue
+
+        parent_indent, siblings = t
 
         commentid = row[0]
 
@@ -38,12 +41,12 @@ def thread(query, reverse_top_level):
             "username": row[3],
             "content": row[4],
             "unixtime": row[5],
-            "indent": row[7],
-            "hidden": 'h' in row[6],
-            "hidden_by": row[8],
+            "indent": parent_indent + 1,
+            "hidden": row[6],
+            "hidden_by": row[7],
         })
 
-        by_parent[commentid] = []
+        by_parent[commentid] = parent_indent + 1, []
 
     result = []
 
@@ -53,41 +56,56 @@ def thread(query, reverse_top_level):
                 c["hidden"] = True
 
             result.append(c)
-            _add_with_descendants(by_parent[c["commentid"]], c["hidden"])
+            _add_with_descendants(by_parent[c["commentid"]][1], c["hidden"])
 
     _add_with_descendants(
-        reversed(by_parent[None]) if reverse_top_level else by_parent[None],
+        reversed(by_parent[None][1]) if reverse_top_level else by_parent[None][1],
         hidden=False,
     )
 
     return result
 
 
-def select(userid, submitid=None, charid=None, journalid=None):
+def select(userid, submitid=None, charid=None, journalid=None, updateid=None):
+    is_hidden = "cm.settings ~ 'h'"
+
     if submitid:
         statement = ["""
             SELECT
                 cm.commentid, cm.parentid, cm.userid, pr.username,
-                cm.content, cm.unixtime, cm.settings, cm.indent,
-                cm.hidden_by
+                cm.content, cm.unixtime, cm.settings ~ 'h', cm.hidden_by
             FROM comments cm
                 INNER JOIN profile pr USING (userid)
             WHERE cm.target_sub = %d
         """ % (submitid,)]
     else:
+        unixtime = "cm.unixtime"
+
+        if submitid:
+            table = "submitcomment"
+        elif charid:
+            table = "charcomment"
+        elif journalid:
+            table = "journalcomment"
+        elif updateid:
+            table = "siteupdatecomment"
+            is_hidden = "cm.hidden_at IS NOT NULL"
+            unixtime = "EXTRACT(EPOCH FROM cm.created_at)::int8 + %i" % (UNIXTIME_OFFSET,)
+        else:
+            raise WeasylError("Unexpected")
+
         statement = ["""
             SELECT
                 cm.commentid, cm.parentid, cm.userid, pr.username,
-                cm.content, cm.unixtime, cm.settings, cm.indent,
-                cm.hidden_by
-            FROM %scomment cm
+                cm.content, %s, %s, cm.hidden_by
+            FROM %s cm
                 INNER JOIN profile pr USING (userid)
             WHERE cm.targetid = %i
-        """ % ("submit" if submitid else "char" if charid else "journal", d.get_targetid(submitid, charid, journalid))]
+        """ % (unixtime, is_hidden, table, d.get_targetid(submitid, charid, journalid, updateid))]
 
     # moderators get to view hidden comments
     if userid not in staff.MODS:
-        statement.append(" AND cm.settings !~ 'h'")
+        statement.append(" AND NOT (%s)" % (is_hidden,))
 
     if userid:
         statement.append(m.MACRO_IGNOREUSER % (userid, "cm"))
@@ -99,42 +117,57 @@ def select(userid, submitid=None, charid=None, journalid=None):
     return result
 
 
-# TODO(kailys): use ORM argument after comment tables are merged.
-def insert(userid, submitid=None, charid=None, journalid=None, parentid=None, content=None):
-    if not submitid and not charid and not journalid:
+def insert(userid, submitid=None, charid=None, journalid=None, updateid=None, parentid=None, content=None):
+    if not submitid and not charid and not journalid and not updateid:
         raise WeasylError("Unexpected")
     elif not content:
         raise WeasylError("commentInvalid")
 
     # Determine indent and parentuserid
     if parentid:
-        query = d.execute("SELECT userid, indent FROM %s WHERE commentid = %i",
-                          ["comments" if submitid else "charcomment" if charid else "journalcomment", parentid],
-                          options="single")
+        if updateid:
+            parentuserid = d.engine.scalar("SELECT userid FROM siteupdatecomment WHERE commentid = %(parent)s", parent=parentid)
 
-        if not query:
-            raise WeasylError("Unexpected")
+            if parentuserid is None:
+                raise WeasylError("Unexpected")
+        else:
+            query = d.execute("SELECT userid, indent FROM %s WHERE commentid = %i",
+                              ["comments" if submitid else "charcomment" if charid else "journalcomment", parentid],
+                              options="single")
 
-        indent = query[1] + 1
-        parentuserid = query[0]
+            if not query:
+                raise WeasylError("Unexpected")
+
+            indent = query[1] + 1
+            parentuserid = query[0]
+    elif updateid:
+        parentid = None  # parentid == 0
+        parentuserid = None
     else:
         indent = 0
         parentuserid = None
 
-    # Determine otherid
-    otherid = d.execute("SELECT userid FROM %s WHERE %s = %i AND settings !~ 'h'",
-                        ["submission", "submitid", submitid] if submitid else
-                        ["character", "charid", charid] if charid else
-                        ["journal", "journalid", journalid], options="element")
+    if updateid:
+        otherid = d.engine.scalar("SELECT userid FROM siteupdate WHERE updateid = %(update)s", update=updateid)
 
-    # Check permissions
-    if not otherid:
-        raise WeasylError("submissionRecordMissing")
-    elif ignoreuser.check(otherid, userid):
-        raise WeasylError("pageOwnerIgnoredYou")
-    elif ignoreuser.check(userid, otherid):
-        raise WeasylError("youIgnoredPageOwner")
-    elif parentuserid and ignoreuser.check(parentuserid, userid):
+        if not otherid:
+            raise WeasylError("submissionRecordMissing")
+    else:
+        # Determine otherid
+        otherid = d.execute("SELECT userid FROM %s WHERE %s = %i AND settings !~ 'h'",
+                            ["submission", "submitid", submitid] if submitid else
+                            ["character", "charid", charid] if charid else
+                            ["journal", "journalid", journalid], options="element")
+
+        # Check permissions
+        if not otherid:
+            raise WeasylError("submissionRecordMissing")
+        elif ignoreuser.check(otherid, userid):
+            raise WeasylError("pageOwnerIgnoredYou")
+        elif ignoreuser.check(userid, otherid):
+            raise WeasylError("youIgnoredPageOwner")
+
+    if parentuserid and ignoreuser.check(parentuserid, userid):
         raise WeasylError("replyRecipientIgnoredYou")
     elif parentuserid and ignoreuser.check(userid, parentuserid):
         raise WeasylError("youIgnoredReplyRecipient")
@@ -148,6 +181,16 @@ def insert(userid, submitid=None, charid=None, journalid=None, parentid=None, co
             .values(userid=userid, target_sub=submitid, parentid=parentid or None,
                     content=content, unixtime=arrow.utcnow(), indent=indent)
             .returning(co.c.commentid))
+    elif updateid:
+        commentid = d.engine.scalar(
+            "INSERT INTO siteupdatecomment (userid, targetid, parentid, content)"
+            " VALUES (%(user)s, %(update)s, %(parent)s, %(content)s)"
+            " RETURNING commentid",
+            user=userid,
+            update=updateid,
+            parent=parentid,
+            content=content,
+        )
     else:
         commentid = d.execute(
             "INSERT INTO %s (userid, targetid, parentid, "
@@ -160,7 +203,7 @@ def insert(userid, submitid=None, charid=None, journalid=None, parentid=None, co
 
     # Create notification
     if parentid and (userid != parentuserid):
-        welcome.commentreply_insert(userid, commentid, parentuserid, parentid, submitid, charid, journalid)
+        welcome.commentreply_insert(userid, commentid, parentuserid, parentid, submitid, charid, journalid, updateid)
     elif not parentid:
         # build a list of people this comment should notify
         # circular imports are cool and fun
@@ -180,7 +223,7 @@ def insert(userid, submitid=None, charid=None, journalid=None, parentid=None, co
         notified.discard(userid)
 
         for other in notified:
-            welcome.comment_insert(userid, commentid, other, submitid, charid, journalid)
+            welcome.comment_insert(userid, commentid, other, submitid, charid, journalid, updateid)
 
     d.metric('increment', 'comments')
 
@@ -188,20 +231,24 @@ def insert(userid, submitid=None, charid=None, journalid=None, parentid=None, co
 
 
 def remove(userid, feature=None, commentid=None):
-
-    if feature not in ["submit", "char", "journal"]:
+    if feature not in ["submit", "char", "journal", "siteupdate"]:
         raise WeasylError("Unexpected")
 
     if feature == 'submit':
         query = d.execute(
-            "SELECT userid, target_sub FROM comments WHERE commentid = %i AND settings !~ 'h'",
+            "SELECT userid, target_sub FROM comments WHERE commentid = %i AND target_sub IS NOT NULL AND settings !~ 'h'",
             [commentid], ["single"])
+    elif feature == 'siteupdate':
+        query = d.engine.execute(
+            "SELECT userid, targetid FROM siteupdatecomment WHERE commentid = %(comment)s AND hidden_at IS NULL",
+            comment=commentid,
+        ).first()
     else:
         query = d.execute(
             "SELECT userid, targetid FROM %scomment WHERE commentid = %i AND settings !~ 'h'",
             [feature, commentid], ["single"])
 
-    if not query or query[1] is None:
+    if not query:
         raise WeasylError("RecordMissing")
 
     target_table = {
@@ -210,10 +257,13 @@ def remove(userid, feature=None, commentid=None):
         "journal": "journal",
     }
 
-    owner = d.execute(
-        "SELECT userid FROM %s WHERE %sid = %i",
-        [target_table[feature], feature, query[1]], ["single"])
-    is_owner = userid == owner[0]
+    if feature == 'siteupdate':
+        is_owner = False
+    else:
+        owner = d.execute(
+            "SELECT userid FROM %s WHERE %sid = %i",
+            [target_table[feature], feature, query[1]], ["single"])
+        is_owner = userid == owner[0]
 
     if not is_owner and userid not in staff.MODS:
         if userid != query[0]:
@@ -238,6 +288,12 @@ def remove(userid, feature=None, commentid=None):
         d.execute(
             "UPDATE comments SET settings = settings || 'h', hidden_by = %i WHERE commentid = %i",
             [userid, commentid])
+    elif feature == 'siteupdate':
+        d.engine.execute(
+            "UPDATE siteupdatecomment SET hidden_at = now(), hidden_by = %(hidden_by)s WHERE commentid = %(comment)s",
+            comment=commentid,
+            hidden_by=userid,
+        )
     else:
         d.execute(
             "UPDATE %scomment SET settings = settings || 'h', hidden_by = %i WHERE commentid = %i",

assets/scss/site.scss
@@ -3224,6 +3224,12 @@ img + #detail-art-text {
     clear: both;
 }
 
+#site-update-comments {
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 66em;
+}
+
 #detail-report {
     padding-bottom: 3em;
 }

libweasyl/libweasyl/alembic/versions/9270baf773a5_add_comments_to_site_updates.py
@@ -1,0 +1,42 @@
+"""Add comments to site updates
+
+Revision ID: 9270baf773a5
+Revises: 7f0e262d6370
+Create Date: 2019-01-21 16:41:17.360120
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '9270baf773a5'
+down_revision = '7f0e262d6370'
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+def upgrade():
+    # ### commands auto generated by Alembic - please adjust! ###
+    op.create_table('siteupdatecomment',
+    sa.Column('commentid', sa.Integer(), nullable=False),
+    sa.Column('userid', sa.Integer(), nullable=False),
+    sa.Column('targetid', sa.Integer(), nullable=False),
+    sa.Column('parentid', sa.Integer(), nullable=True),
+    sa.Column('content', sa.String(length=10000), nullable=False),
+    sa.Column('created_at', postgresql.TIMESTAMP(timezone=True), server_default=sa.text(u'now()'), nullable=False),
+    sa.Column('hidden_at', postgresql.TIMESTAMP(timezone=True), nullable=True),
+    sa.Column('hidden_by', sa.Integer(), nullable=True),
+    sa.CheckConstraint(u'hidden_by IS NULL OR hidden_at IS NOT NULL', name='siteupdatecomment_hidden_check'),
+    sa.ForeignKeyConstraint(['hidden_by'], ['login.userid'], name='siteupdatecomment_hidden_by_fkey', ondelete='SET NULL'),
+    sa.ForeignKeyConstraint(['targetid', 'parentid'], ['siteupdatecomment.targetid', 'siteupdatecomment.commentid'], name='siteupdatecomment_parentid_fkey'),
+    sa.ForeignKeyConstraint(['targetid'], ['siteupdate.updateid'], name='siteupdatecomment_targetid_fkey'),
+    sa.ForeignKeyConstraint(['userid'], ['login.userid'], name='siteupdatecomment_userid_fkey'),
+    sa.PrimaryKeyConstraint('commentid'),
+    sa.UniqueConstraint('targetid', 'commentid')
+    )
+    # ### end Alembic commands ###
+
+
+def downgrade():
+    # ### commands auto generated by Alembic - please adjust! ###
+    op.drop_table('siteupdatecomment')
+    # ### end Alembic commands ###

libweasyl/libweasyl/models/tables.py
@@ -1,5 +1,5 @@
 from sqlalchemy import (
-    MetaData, Table, Column, CheckConstraint, ForeignKeyConstraint, Index,
+    MetaData, Table, Column, CheckConstraint, ForeignKeyConstraint, UniqueConstraint, Index,
     Integer, String, Text, SMALLINT, text, DateTime, func, Boolean)
 from sqlalchemy.dialects.postgresql import ARRAY, JSONB, TIMESTAMP
 
@@ -729,6 +729,28 @@ siteupdate = Table(
     Column('content', Text(), nullable=False),
     Column('unixtime', WeasylTimestampColumn(), nullable=False),
     default_fkey(['userid'], ['login.userid'], name='siteupdate_userid_fkey'),
+)
+
+
+siteupdatecomment = Table(
+    'siteupdatecomment', metadata,
+    Column('commentid', Integer(), primary_key=True, nullable=False),
+    Column('userid', Integer(), nullable=False),
+    Column('targetid', Integer(), nullable=False),
+    Column('parentid', Integer(), nullable=True),
+    Column('content', String(length=10000), nullable=False),
+    Column('created_at', TIMESTAMP(timezone=True), nullable=False, server_default=func.now()),
+    Column('hidden_at', TIMESTAMP(timezone=True), nullable=True),
+    Column('hidden_by', Integer(), nullable=True),
+    ForeignKeyConstraint(['targetid'], ['siteupdate.updateid'], name='siteupdatecomment_targetid_fkey'),
+    ForeignKeyConstraint(
+        ['targetid', 'parentid'],
+        ['siteupdatecomment.targetid', 'siteupdatecomment.commentid'],
+        name='siteupdatecomment_parentid_fkey'),
+    ForeignKeyConstraint(['userid'], ['login.userid'], name='siteupdatecomment_userid_fkey'),
+    ForeignKeyConstraint(['hidden_by'], ['login.userid'], name='siteupdatecomment_hidden_by_fkey', ondelete='SET NULL'),
+    CheckConstraint("hidden_by IS NULL OR hidden_at IS NOT NULL", name='siteupdatecomment_hidden_check'),
+    UniqueConstraint('targetid', 'commentid'),
 )
 
 

templates/common/comment_form.html
@@ -9,6 +9,8 @@ $code:
     feature = "charid"
   elif feature == "journal":
     feature = "journalid"
+  elif feature == "siteupdate":
+    feature = "updateid"
 
 <form class="comment-form" action="/submit/${'shout' if feature in ['userid', 'staffnotes'] else 'comment'}" method="post">
   $:{CSRF()}

templates/etc/index.html
@@ -97,6 +97,10 @@ $ _GRID_ITEM = COMPILE("common/thumbnail_grid_item.html")
       <div class="formatted-content">
         $:{MARKDOWN(update['content'])}
       </div>
+
+      $ comment_count = update.get('comment_count')
+      $if comment_count is not None:
+        <a href="/site-updates/${update['updateid']}" class="more more-block"><i>View</i> <span>This Update</span> <i>and</i> <span>${comment_count} Comment${'' if comment_count == 1 else 's'}</span></a>
     </div>
 
   <div id="hc-critique" class="clear">

templates/etc/site_update.html
@@ -1,4 +1,4 @@
-$def with (update)
+$def with (myself, update, comments)
 $ owner = update.get_display_owner()
 <div id="home-content" class="content">
   <div class="constrained">
@@ -17,5 +17,14 @@ $ owner = update.get_display_owner()
     <div class="formatted-content" style="margin-top: 1em">
       $:{MARKDOWN(update.content)}
     </div>
+  </div>
+
+  <div id="site-update-comments">
+    $if comments:
+      <div class="constrained"><h3>Comments</h3></div>
+      $:{RENDER("common/comment_thread.html", ["detail_comments", comments, "siteupdate", myself, update.updateid, update.userid])}
+    $if myself:
+      <div class="constrained"><h3>Leave a Comment</h3></div>
+      $:{RENDER("common/comment_form.html", [myself, update.updateid, "siteupdate"])}
   </div>
 </div>

templates/message/notifications.html
@@ -13,7 +13,8 @@ $code:
     elif x in [4010, 4015]:       return [4010, 4015]
     elif x == 4016:               return [4016]
     elif x in [4020, 4025, 4050]: return [4020, 4025, 4050]
-    elif x in [4030, 4035]:       return [4030, 4035]
+    elif x in [4030, 4035, 4060, 4065]:
+      return [4030, 4035, 4060, 4065]
     elif x in [4040, 4045]:       return [4040, 4045]
     else:                         return []
 
@@ -30,7 +31,8 @@ $code:
     elif x in [4010, 4015]:       return "Shouts"
     elif x == 4016:               return "Staff Notes"
     elif x in [4020, 4025, 4050]: return "Submission Comments"
-    elif x in [4030, 4035]:       return "Journal Comments"
+    elif x in [4030, 4035, 4060, 4065]:
+      return "Journal Comments"
     elif x in [4040, 4045]:       return "Character Comments"
     else:                         return "Miscellaneous"
 
@@ -83,7 +85,7 @@ $:{RENDER("common/stage_title.html", ["Notifications", "Messages", 0])}
               if 'username' in i:
                 item_user = '<a href="/~' + LOGIN(i['username']) + '" class="username">' + escape_(i['username']) + '</a>'
 
-              if 'submitid' in i or 'journalid' in i or 'charid' in i:
+              if 'submitid' in i or 'journalid' in i or 'charid' in i or 'updateid' in i:
                 if 'submitid' in i:
                   folder = 'submission'
                   id = i['submitid']
@@ -93,8 +95,12 @@ $:{RENDER("common/stage_title.html", ["Notifications", "Messages", 0])}
                 elif 'charid' in i:
                   folder = 'character'
                   id = i['charid']
+                elif 'updateid' in i:
+                  folder = 'site-updates'
+                  id = i['updateid']
                 if 'title' in i:
-                  item_base_url = '/%s/%i/%s' % (folder, id, SLUG(i['title']))
+                  slug = '' if 'updateid' in i else '/%s' % (SLUG(i['title']),)
+                  item_base_url = '/%s/%i%s' % (folder, id, slug)
                   item_link = '<a href="' + item_base_url + '">' + escape_(i['title']) + '</a>'
                   if 'commentid' in i:
                     comment_url = lambda id: item_base_url + '#cid' + STR(id)
@@ -185,6 +191,12 @@ $:{RENDER("common/stage_title.html", ["Notifications", "Messages", 0])}
             $elif i['type'] == 4050:
               $# comment on collected item
               $:{item_user} left a $:{item_comment} on your collected submission $:{item_link}.
+            $elif i['type'] == 4060:
+              $# site update comment
+              $:{item_user} left a $:{item_comment} on your site update $:{item_link}.
+            $elif i['type'] == 4065:
+              $# site update comment reply
+              $:{item_user} left a $:{item_reply} to $:{item_your_comment} on the site update $:{item_link}.
             $else:
               ${i}
             <span class="date color-lighter">${DATE(i['unixtime'])} <em>at</em> ${TIME(i['unixtime'])}</span>

commit 2be2f3af833a79c28d677e749f299d839a6fdcba

Remove unreachable (and wrong) code
Introduced in c6455b4cd8c76203344d0403a1a13dca694c07c2.

weasyl/comment.py
@@ -81,9 +81,7 @@ def select(userid, submitid=None, charid=None, journalid=None, updateid=None):
     else:
         unixtime = "cm.unixtime"
 
-        if submitid:
-            table = "submitcomment"
-        elif charid:
+        if charid:
             table = "charcomment"
         elif journalid:
             table = "journalcomment"
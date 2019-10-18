91d6fdde3cb566a717e927356117f07b9b81da6d
Kyra Kitsune <kfkitsune@users.noreply.github.com>
2019-10-17 23:20:19 -0400

    Update Travis CI configuration
    Drop Python 2.7; add Python 3.6, 3.7, and 3.8 as targeted build environments.

--- .travis-install.sh
+++ .travis-install.sh
@@ -1,8 +1,4 @@
 #!/bin/sh -eux
-if [ "$(python -V 2>&1)" ">" "Python 3" ]; then
-    weasyl_reqs=
-else
-    weasyl_reqs='-r etc/requirements.txt -c etc/requirements.txt -e .'
-    make setup
-fi
+weasyl_reqs='-r etc/requirements.txt -c etc/requirements.txt -e .'
+make setup
 pip install -U $weasyl_reqs -e './libweasyl[development]' codecov==2.0.15 pytest-cov==2.7.1

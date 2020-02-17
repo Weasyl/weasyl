8eb489c979ba4707811ac4b6ce61ed90c268e034
sl1-1 <sl1.1@outlook.com>
2020-02-17 13:41:20 -0500

    remove use of storify in controllers/marketplace.py

--- weasyl/controllers/marketplace.py
+++ weasyl/controllers/marketplace.py
@@ -6,18 +6,23 @@ from weasyl import define, media, commishinfo
 
 
 def search_(request):
-    form = request.web_input(q="", min="", max="", currency="", pc="", c="", o="")
     limit = 30
-    offset = define.get_int(form.o)
-    commishclass = form.pc if form.pc else form.c
+    offset = define.get_int(request.params.get('o', None))
+    pc = request.params.get('pc', '')
+    c = request.params.get('c', '')
+    commishclass = pc if pc else c
     commishclass = commishclass.lower()
+    q = request.params.get('q', '')
+    min_value = request.params.get('min', '')
+    max_value = request.params.get('max', '')
+    currency = request.params.get('currency', '')
 
     results = commishinfo.select_commissionable(request.userid,
-                                                form.q,
+                                                q,
                                                 commishclass,
-                                                commishinfo.parse_currency(form.min),
-                                                commishinfo.parse_currency(form.max),
-                                                form.currency,
+                                                commishinfo.parse_currency(min_value),
+                                                commishinfo.parse_currency(max_value),
+                                                currency,
                                                 offset,
                                                 limit * 2,)
     rcount = len(results)
@@ -25,6 +30,7 @@ def search_(request):
     media.populate_with_user_media(results)
     prev_index = None if offset == 0 else offset - limit if offset - limit > 0 else 0
     next_index = offset + limit if rcount - limit > 0 else None
+    form = {'pc': pc, 'c': c, 'q': q, 'min': min_value, 'max': max_value, 'currency': currency}
     return Response(define.webpage(request.userid, "etc/marketplace.html",
                     [results, form, commishinfo.CURRENCY_CHARMAP, commishinfo.PRESET_COMMISSION_CLASSES,
                      prev_index, next_index]))

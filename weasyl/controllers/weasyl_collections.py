947fa7802b93239c0cba401c873b255d3b714ac3
sl1-1 <sl1.1@outlook.com>
2020-02-17 16:49:10 -0500

    remove dependency on storify from controllers/weasyl_collections.py

--- weasyl/controllers/weasyl_collections.py
+++ weasyl/controllers/weasyl_collections.py
@@ -21,11 +21,9 @@ def collection_options_get_(request):
 @login_required
 @token_checked
 def collection_options_post_(request):
-    form = request.web_input(allow_request="", allow_notification="")
-
     jsonb_settings = define.get_profile_settings(request.userid)
-    jsonb_settings.allow_collection_requests = form.allow_request
-    jsonb_settings.allow_collection_notifs = form.allow_notification
+    jsonb_settings.allow_collection_requests = 'allow_request' in request.params
+    jsonb_settings.allow_collection_notifs = 'allow_notification' in request.params
 
     profile.edit_preferences(request.userid, jsonb_settings=jsonb_settings)
     raise HTTPSeeOther(location="/control")
@@ -34,21 +32,20 @@ def collection_options_post_(request):
 @login_required
 @token_checked
 def collection_offer_(request):
-    form = request.web_input(submitid="", username="")
-    form.otherid = profile.resolve(None, None, form.username)
-    form.submitid = int(form.submitid)
+    otherid = profile.resolve(None, None, request.params.get('username'))
+    submitid = int(request.params.get('submitid'))
 
-    if not form.otherid:
+    if not otherid:
         raise WeasylError("userRecordMissing")
-    if request.userid == form.otherid:
+    if request.userid == otherid:
         raise WeasylError("cannotSelfCollect")
 
-    collection.offer(request.userid, form.submitid, form.otherid)
+    collection.offer(request.userid, submitid, otherid)
     return Response(define.errorpage(
         request.userid,
         "**Success!** Your collection offer has been sent "
         "and the recipient may now add this submission to their gallery.",
-        [["Go Back", "/submission/%i" % (form.submitid,)], ["Return to the Home Page", "/index"]]))
+        [["Go Back", "/submission/%i" % (submitid,)], ["Return to the Home Page", "/index"]]))
 
 
 @login_required
@@ -57,29 +54,27 @@ def collection_request_(request):
     if not define.is_vouched_for(request.userid):
         raise WeasylError("vouchRequired")
 
-    form = request.web_input(submitid="")
-    form.submitid = int(form.submitid)
-    form.otherid = define.get_ownerid(submitid=form.submitid)
+    submitid = int(request.params.get('submitid'))
+    otherid = define.get_ownerid(submitid=submitid)
 
-    if not form.otherid:
+    if not otherid:
         raise WeasylError("userRecordMissing")
-    if request.userid == form.otherid:
+    if request.userid == otherid:
         raise WeasylError("cannotSelfCollect")
 
-    collection.request(request.userid, form.submitid, form.otherid)
+    collection.request(request.userid, submitid, otherid)
     return Response(define.errorpage(
         request.userid,
         "**Success!** Your collection request has been sent. "
         "The submission author may approve or reject this request.",
-        [["Go Back", "/submission/%i" % (form.submitid,)], ["Return to the Home Page", "/index"]]))
+        [["Go Back", "/submission/%i" % (submitid,)], ["Return to the Home Page", "/index"]]))
 
 
 @login_required
 @token_checked
 def collection_remove_(request):
-    form = request.web_input(submissions=[])
     # submissions input format: "submissionID;collectorID"
-    submissions = [int(x.split(";")[0]) for x in form.submissions]
+    submissions = [int(x.split(";")[0]) for x in request.getall('submissions')]
 
     collection.remove(request.userid, submissions)
     raise HTTPSeeOther(location="/manage/collections")

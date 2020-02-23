c96fc5ce8b28e61a473ef81086a033cb6f82bfcb
sl1-1 <sl1.1@outlook.com>
2020-02-23 15:42:22 -0500

    remove inline_json, as it is no longer needed.

--- libweasyl/libweasyl/html.py
+++ libweasyl/libweasyl/html.py
@@ -4,7 +4,6 @@ Utilities for dealing with HTML.
 Specifically, utilities for creating HTML and utilities for removing HTML.
 """
 
-import json
 
 try:
     from html.parser import HTMLParser
@@ -45,16 +44,3 @@ def strip_html(markdown):
     parser = Parser()
     parser.feed(markdown)
     return "".join(parser.text_parts)
-
-
-def inline_json(obj):
-    """
-    Format a python object as JSON for inclusion in HTML.
-
-    Parameters:
-        obj: A python object that can be converted to JSON.
-
-    Returns:
-        An escaped :term:`native string` of JSON.
-    """
-    return json.dumps(obj).replace("</", r"<\/").replace("<!--", r"<\!--")

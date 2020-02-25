0fac4f84ba6577bd864a4a4e5ada7b9cba4c6f4f
Charmander <~@charmander.me>
2020-02-24 21:37:59 -0800

    Remove unused variable in test

--- weasyl/test/web/test_submissions.py
+++ weasyl/test/web/test_submissions.py
@@ -50,7 +50,7 @@ def test_rating_accessibility(app, age):
 
 @pytest.mark.usefixtures('db', 'no_csrf')
 def test_gif_thumbnail_static(app, submission_user):
-    sub = create_visual(
+    create_visual(
         app, submission_user,
         submitfile=webtest.Upload('loader.gif', read_asset('img/loader.gif'), 'image/gif'),
     )

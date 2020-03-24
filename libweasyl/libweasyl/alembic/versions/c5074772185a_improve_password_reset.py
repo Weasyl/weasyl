1705e4bffac2c1e99a07cf2489210bbeab065f3b
Charmander <~@charmander.me>
2020-03-24 11:46:48 -0700

    Improve password reset
    - always send an e-mail so people aren’t left guessing as to whether something is eventually coming (and so various behaviours are independent of whether an account with the given e-mail address exists on the site)
    
    - look up e-mail case-insensitively (but prefer case-sensitive for backwards compatibility – there are 700+ e-mail addresses with multiple associated accounts because registration is case-sensitive! registration being `<input type="email">` but password reset being `<input type="text">` might have made this even more visible than usual on mobile.)
    
    - look up e-mail at reset time so that a password reset request sent to an account’s previous address doesn’t work (even though the window was only one hour)
    
    - drop 5-minute timer that starts on visiting the link, because it doesn’t do much for security and could be incredibly frustrating
    
    - allow requesting a reset and performing the reset on different IP addresses, because people’s IP addresses can change and they can use multiple devices
    
    - don’t require people to reenter their username and e-mail address, to make use of the password reset less annoying
    
    - further to the previous, *show* the username so the feature can also act as a forgotten username lookup
    
    - sidestep any potential side channel attacks on the token (and questions of whether keeping the tokens in the database is a risk) by hashing tokens and therefore not knowing them
    
    - reduce the length and complexity of tokens, just in case someone needs to retype one
    
    - log password resets
    
    An important future improvement is to make this at least as resistant to abuse as the signup page, so we should start by applying the CAPTCHA to it as soon as the server is able to reach Google to validate them again.
    
    One new risk this introduces is that someone with access to the mailbox will be able to view the owner’s Weasyl username undetected by them, but I think it’s acceptable.
    
    Letting a user skip the login screen after changing their password would probably be helpful, but with 2FA and status checks having to be moved around to accomplish that, it seems best to leave that for later.

---
+++ libweasyl/libweasyl/alembic/versions/c5074772185a_improve_password_reset.py
@@ -0,0 +1,51 @@
+"""Improve password reset
+
+Revision ID: c5074772185a
+Revises: 3accc3d526ba
+Create Date: 2020-03-28 03:11:19.300735
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'c5074772185a'
+down_revision = '3accc3d526ba'
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+def upgrade():
+    op.drop_table('forgotpassword')
+    op.create_table('forgotpassword',
+    sa.Column('token_sha256', postgresql.BYTEA(), nullable=False),
+    sa.Column('email', sa.String(length=254), nullable=False),
+    sa.Column('created_at', postgresql.TIMESTAMP(timezone=True), server_default=sa.text(u'now()'), nullable=False),
+    sa.PrimaryKeyConstraint('token_sha256')
+    )
+    op.create_index('ind_forgotpassword_created_at', 'forgotpassword', ['created_at'], unique=False)
+    op.create_table('user_events',
+    sa.Column('eventid', sa.Integer(), nullable=False),
+    sa.Column('userid', sa.Integer(), nullable=False),
+    sa.Column('event', sa.String(length=100), nullable=False),
+    sa.Column('data', postgresql.JSONB(astext_type=sa.Text()), nullable=False),
+    sa.Column('occurred', postgresql.TIMESTAMP(timezone=True), server_default=sa.text(u'now()'), nullable=False),
+    sa.ForeignKeyConstraint(['userid'], ['login.userid'], ),
+    sa.PrimaryKeyConstraint('eventid')
+    )
+    op.create_index('ind_user_events_userid_eventid', 'user_events', ['userid', 'eventid'], unique=False)
+
+
+def downgrade():
+    op.drop_table('forgotpassword')
+    op.create_table('forgotpassword',
+    sa.Column('set_time', sa.INTEGER(), autoincrement=False, nullable=False),
+    sa.Column('address', sa.TEXT(), autoincrement=False, nullable=False),
+    sa.Column('link_time', sa.INTEGER(), server_default=sa.text(u'0'), autoincrement=False, nullable=False),
+    sa.Column('userid', sa.INTEGER(), autoincrement=False, nullable=False),
+    sa.Column('token', sa.VARCHAR(length=100), autoincrement=False, nullable=False),
+    sa.ForeignKeyConstraint(['userid'], [u'login.userid'], name=u'forgotpassword_userid_fkey', onupdate=u'CASCADE', ondelete=u'CASCADE'),
+    sa.UniqueConstraint('userid', name=u'forgotpassword_userid_key')
+    )
+    op.drop_index('ind_user_events_userid_eventid', table_name='user_events')
+    op.drop_table('user_events')
+    op.drop_index('ind_forgotpassword_created_at', table_name='forgotpassword')
--- weasyl/test/resetpassword/test_prepare.py
+++ weasyl/test/resetpassword/test_prepare.py
@@ -1,87 +1,69 @@
 from __future__ import absolute_import
 
+import hashlib
+
 import pytest
-import arrow
 
 from weasyl import resetpassword
 from weasyl import define as d
 from weasyl.test import db_utils
-from weasyl.test.utils import Bag
 
 
 @pytest.mark.usefixtures('db')
-def test_false_returned_if_token_does_not_exist():
+def test_none_returned_if_token_does_not_exist():
     token = "testtokentesttokentesttokentesttokentesttokentesttokentesttokentesttokentesttokentesttokentest000000"
-    assert not resetpassword.prepare(token)
+    assert resetpassword.prepare(token) is None
 
 
 @pytest.mark.usefixtures('db')
-def test_true_returned_if_token_exists():
-    user_id = db_utils.create_user(username='checktoken0002')
-    token = "testtokentesttokentesttokentesttokentesttokentesttokentesttokentesttokentesttokentesttokentest000001"
+def test_user_info_returned_if_token_exists():
+    email_addr = "test@weasyl.com"
+    username = "checktoken0002"
+    user_id = db_utils.create_user(username=username, email_addr=email_addr)
+    token = "testtokentesttokentesttokentesttokentesttokentesttokentesttokentesttokentesttokentesttokentest000008"
     d.engine.execute(d.meta.tables["forgotpassword"].insert(), {
         "userid": user_id,
-        "token": token,
-        "set_time": d.get_time(),
-        "link_time": 0,
-        "address": d.get_address(),
+        "token_sha256": hashlib.sha256(token.encode("ascii")).digest(),
+        "email": email_addr,
     })
-    assert resetpassword.prepare(token)
+    assert dict(resetpassword.prepare(token)) == {
+        "userid": user_id,
+        "email": email_addr,
+        "username": username,
+    }
 
 
 @pytest.mark.usefixtures('db')
-def test_stale_records_get_deleted_when_function_is_called():
-    token_store = []
-    for i in range(20):
-        user_name = "testPrepare%d" % (i,)
-        email_addr = "test%d@weasyl.com" % (i,)
-        user_id = db_utils.create_user(email_addr=email_addr, username=user_name)
-        form_for_request = Bag(email=email_addr, username=user_name, day=arrow.now().day,
-                               month=arrow.now().month, year=arrow.now().year)
-        resetpassword.request(form_for_request)
-        pw_reset_token = d.engine.scalar("SELECT token FROM forgotpassword WHERE userid = %(id)s", id=user_id)
-        token_store.append(pw_reset_token)
-    # Set 5 tokens to be two hours old (0,5) (7200)
-    for i in range(0, 5):
-        d.engine.execute("UPDATE forgotpassword SET set_time = %(time)s WHERE token = %(token)s",
-                         time=d.get_time() - 7200, token=token_store[i])
-    # Set 5 tokens to be 30 minutes old (5,10) (1800)
-    for i in range(5, 10):
-        d.engine.execute("UPDATE forgotpassword SET set_time = %(time)s WHERE token = %(token)s",
-                         time=d.get_time() - 1800, token=token_store[i])
-    # Set 5 tokens to be 10 minutes old for the last visit time (10,15) (600)
-    for i in range(10, 15):
-        d.engine.execute("UPDATE forgotpassword SET link_time = %(time)s WHERE token = %(token)s",
-                         time=d.get_time() - 600, token=token_store[i])
-    # Set 5 tokens to be 2 minutes old for the last visit time (10,15) (120)
-    for i in range(15, 20):
-        d.engine.execute("UPDATE forgotpassword SET link_time = %(time)s WHERE token = %(token)s",
-                         time=d.get_time() - 120, token=token_store[i])
-    # This range should be invalid (set_time > 3600)
-    for i in range(0, 5):
-        assert not resetpassword.prepare(token_store[i])
-    # This range should still be valid (set_time < 3600)
-    for i in range(5, 10):
-        assert resetpassword.prepare(token_store[i])
-    # This range should be invalid (link_time > 300)
-    for i in range(10, 15):
-        assert not resetpassword.prepare(token_store[i])
-    # This range should still be valid (link_time < 300)
-    for i in range(15, 20):
-        assert resetpassword.prepare(token_store[i])
-
-
-@pytest.mark.usefixtures('db')
-def test_link_time_field_is_updated_when_valid_token_supplied_to_function():
-    user_name = "test"
+def test_unregistered():
     email_addr = "test@weasyl.com"
-    user_id = db_utils.create_user(email_addr=email_addr, username=user_name)
-    form_for_request = Bag(email=email_addr, username=user_name, day=arrow.now().day,
-                           month=arrow.now().month, year=arrow.now().year)
-    resetpassword.request(form_for_request)
-    pw_reset_token = d.engine.scalar("SELECT token FROM forgotpassword WHERE userid = %(id)s", id=user_id)
-    link_time = d.engine.scalar("SELECT link_time FROM forgotpassword WHERE token = %(token)s", token=pw_reset_token)
-    assert link_time == 0
-    resetpassword.prepare(pw_reset_token)
-    link_time = d.engine.scalar("SELECT link_time FROM forgotpassword WHERE token = %(token)s", token=pw_reset_token)
-    assert link_time >= d.get_time() - 10
+    user_id = db_utils.create_user(username='checktoken0002')
+    token = "testtokentesttokentesttokentesttokentesttokentesttokentesttokentesttokentesttokentesttokentest000008"
+    d.engine.execute(d.meta.tables["forgotpassword"].insert(), {
+        "userid": user_id,
+        "token_sha256": hashlib.sha256(token.encode("ascii")).digest(),
+        "email": email_addr,
+    })
+    assert resetpassword.prepare(token) == resetpassword.Unregistered(email=email_addr)
+
+
+@pytest.mark.usefixtures('db')
+def test_stale_records_get_deleted_when_function_is_called(captured_tokens):
+    email_addrs = list(map("test{}@weasyl.com".format, range(10)))
+
+    for email_addr in email_addrs:
+        db_utils.create_user(email_addr=email_addr)
+        resetpassword.request(email=email_addr)
+    # Set 5 tokens to be two hours old (0,5)
+    for i in range(0, 5):
+        d.engine.execute("UPDATE forgotpassword SET created_at = now() - INTERVAL '2 hours' WHERE email = %(email)s",
+                         email=email_addrs[i])
+    # Set 5 tokens to be 30 minutes old (5,10)
+    for i in range(5, 10):
+        d.engine.execute("UPDATE forgotpassword SET created_at = now() - INTERVAL '30 minutes' WHERE email = %(email)s",
+                         email=email_addrs[i])
+    # This range should be invalid (created_at > 3600)
+    for i in range(0, 5):
+        assert not resetpassword.prepare(captured_tokens[email_addrs[i]])
+    # This range should still be valid (created_at < 3600)
+    for i in range(5, 10):
+        assert resetpassword.prepare(captured_tokens[email_addrs[i]])

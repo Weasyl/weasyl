de72c4537cadfa9d7b9ec9b4b87c711309c1b08a
Charmander <~@charmander.me>
2020-04-02 18:28:27 -0700

    Remove last use of weasyl.test.utils.Bag

--- weasyl/test/login/test_get_account_verification_token.py
+++ weasyl/test/login/test_get_account_verification_token.py
@@ -5,7 +5,6 @@ import arrow
 
 from weasyl import login
 from weasyl import define as d
-from weasyl.test.utils import Bag
 
 
 user_name = "test"
@@ -18,33 +17,27 @@ raw_password = "0123456789"
 
 @pytest.mark.usefixtures('db')
 def test_acct_verif_token_returned_if_email_provided_to_function():
-    form = Bag(username=user_name, password='0123456789', passcheck='0123456789',
-               email=email_addr, emailcheck=email_addr,
-               day='12', month='12', year=arrow.now().year - 19)
     d.engine.execute(d.meta.tables["logincreate"].insert(), {
         "token": token,
-        "username": form.username,
-        "login_name": form.username,
+        "username": user_name,
+        "login_name": user_name,
         "hashpass": login.passhash(raw_password),
-        "email": form.email,
+        "email": email_addr,
         "birthday": arrow.Arrow(2000, 1, 1),
     })
-    acct_verification_token = login.get_account_verification_token(email=form.email, username=None)
+    acct_verification_token = login.get_account_verification_token(email=email_addr, username=None)
     assert token == acct_verification_token
 
 
 @pytest.mark.usefixtures('db')
 def test_acct_verif_token_returned_if_username_provided_to_function():
-    form = Bag(username=user_name, password='0123456789', passcheck='0123456789',
-               email=email_addr, emailcheck=email_addr,
-               day='12', month='12', year=arrow.now().year - 19)
     d.engine.execute(d.meta.tables["logincreate"].insert(), {
         "token": token,
-        "username": form.username,
-        "login_name": form.username,
+        "username": user_name,
+        "login_name": user_name,
         "hashpass": login.passhash(raw_password),
-        "email": form.email,
+        "email": email_addr,
         "birthday": arrow.Arrow(2000, 1, 1),
     })
-    acct_verification_token = login.get_account_verification_token(email=None, username=form.username)
+    acct_verification_token = login.get_account_verification_token(email=None, username=user_name)
     assert token == acct_verification_token

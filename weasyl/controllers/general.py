Weasyl/weasyl

commit 07730693e592a80e6b8dd8ab1d57b97ac734d8a5

remove dependency on storify from controllers/general.py

weasyl/controllers/general.py
@@ -101,6 +101,8 @@ def search_(request):
             0,
         ]))
     else:
+        backid = define.get_int(backid)
+        nextid = define.get_int(nextid)
         page.append(define.render("etc/search.html", [
             # Search method
             {"method": "summary"},
@@ -108,9 +110,33 @@ def search_(request):
             None,
             # Search results
             {
-                "submit": search.browse(request.userid, rating, 22, "submit", cat, backid, nextid),
-                "char": search.browse(request.userid, rating, 22, "char", cat, backid, nextid),
-                "journal": search.browse(request.userid, rating, 22, "journal", cat, backid, nextid),
+                "submit": search.browse(
+                    userid=request.userid,
+                    rating=rating,
+                    limit=22,
+                    find="submit",
+                    cat=cat,
+                    backid=backid,
+                    nextid=nextid
+                ),
+                "char": search.browse(
+                    userid=request.userid,
+                    rating=rating,
+                    limit=22,
+                    find="char",
+                    cat=cat,
+                    backid=backid,
+                    nextid=nextid
+                ),
+                "journal": search.browse(
+                    userid=request.userid,
+                    rating=rating,
+                    limit=22,
+                    find="journal",
+                    cat=cat,
+                    backid=backid,
+                    nextid=nextid
+                ),
             },
         ]))
 

weasyl/search.py
@@ -398,9 +398,6 @@ def select(**kwargs):
 
 
 def browse(userid, rating, limit, find, cat, backid, nextid):
-    backid = d.get_int(backid)
-    nextid = d.get_int(nextid)
-
     if find == "char":
         return character.select_list(userid, rating, limit, backid=backid, nextid=nextid)
     elif find == "journal":

commit dc8f3aedb2c46ffd5f883211fe06b92515a36cad

Fix category browsing pagination
Bug introduced in 07730693e592a80e6b8dd8ab1d57b97ac734d8a5.

weasyl/controllers/general.py
@@ -89,8 +89,8 @@ def search_(request):
             limit=66,
             find=find,
             cat=cat,
-            backid=backid,
-            nextid=nextid,
+            backid=define.get_int(backid),
+            nextid=define.get_int(nextid),
         )
 
         meta = {
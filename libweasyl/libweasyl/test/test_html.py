2a7ba881d6aac08ad93bf0f4735472dc29d3369c
Charmander <~@charmander.me>
2022-02-22 21:15:26 -0800

    Remove outdated json monkeypatch in test
    Back from when SimpleJSON was used when available.

--- libweasyl/libweasyl/test/test_html.py
+++ libweasyl/libweasyl/test/test_html.py
@@ -1,10 +1,7 @@
-import json
-
 from libweasyl import html
 
 
-def test_inline_json(monkeypatch):
-    monkeypatch.setattr(html, 'json', json)
+def test_inline_json():
     assert html.inline_json('</script>') == r'"<\/script>"'
     assert html.inline_json('</SCRIPT>') == r'"<\/SCRIPT>"'
     assert html.inline_json('<!--<script>') == r'"<\!--<script>"'

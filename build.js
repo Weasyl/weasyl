6da9fc396b274289d1eb2f308daef2a196c720ee
Charmander <~@charmander.me>
2022-04-18 18:02:42 -0700

    Add JavaScript bundling

--- build.js
+++ build.js
@@ -3,6 +3,7 @@
 const autoprefixer = require('autoprefixer');
 const child_process = require('child_process');
 const crypto = require('crypto');
+const esbuild = require('esbuild');
 const fs = require('fs');
 const path = require('path');
 const postcss = require('postcss');
@@ -166,6 +167,47 @@ const sasscFile = async (relativeInputPath, relativeOutputPath, touch, copyImage
     };
 };
 
+const esbuildFile = async (relativeInputPath, relativeOutputPath, touch, options) => {
+    const result = await esbuild.build({
+        entryPoints: [path.join(ASSETS, relativeInputPath)],
+        write: false,
+        metafile: true,
+        bundle: true,
+        minify: true,
+        target: 'es5',
+        banner: {
+            js: '"use strict";',
+        },
+        ...options,
+    });
+
+    if (result.warnings.length !== 0) {
+        for (const warning of result.warnings) {
+            console.warn(warning);
+        }
+
+        throw new Error('Unexpected warnings');
+    }
+
+    console.log(await esbuild.analyzeMetafile(result.metafile, {verbose: true}));
+
+    const bundleContents = result.outputFiles[0].contents;
+
+    const shortDigest = getShortDigest(
+        crypto.createHash('sha512')
+            .update(bundleContents)
+            .digest()
+    );
+
+    const outputPath = addFilenameSuffix(relativeOutputPath, shortDigest);
+    const outputFullPath = path.join(BUILD, outputPath);
+
+    return {
+        entries: [[relativeOutputPath, outputPath]],
+        work: touch.then(() => fs.promises.writeFile(outputFullPath, bundleContents)),
+    };
+};
+
 const main = async () => {
     const manifestPath = path.join(BUILD, 'rev-manifest.json');
 
@@ -182,9 +224,21 @@ const main = async () => {
         sasscFile('scss/site.scss', 'css/site.css', touch, copyImages),
         sasscFile('scss/help.scss', 'css/help.css', touch, copyImages),
         sasscFile('scss/imageselect.scss', 'css/imageselect.css', touch, copyImages),
+        esbuildFile('js/scripts.js', 'js/scripts.js', touch, {}),
         copyStaticFiles('img/help', touch),
         copyImages,
-        copyStaticFiles('js', touch),
+
+        // libraries
+        copyStaticFile('js/jquery-2.2.4.min.js', touch),
+        copyStaticFile('js/imageselect.js', touch),
+        copyStaticFile('js/marked.js', touch),
+        copyStaticFile('js/zxcvbn.js', touch),
+
+        // site
+        copyStaticFile('js/notification-list.js', touch),
+        copyStaticFile('js/search.js', touch),
+        copyStaticFile('js/tz-autodetect.js', touch),
+        copyStaticFile('js/zxcvbn-check.js', touch),
     ]);
 
     await touch;

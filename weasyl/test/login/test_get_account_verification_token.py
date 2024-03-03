0da3d6a5c6255119221da63b5aeab61dc65af72a
Charmander <~@charmander.me>
2024-03-03 14:15:33 -0800

    Remove unnecessary uses of `Bag` in test

--- weasyl/test/login/test_get_account_verification_token.py
+++ weasyl/test/login/test_get_account_verification_token.py
@@ -3,7 +3,6 @@ import arrow
 
 from weasyl import login
 from weasyl import define as d
-from weasyl.test.utils import Bag
 
 
 user_name = "test"
@@ -16,33 +15,27 @@ raw_password = "0123456789"
 
 @pytest.mark.usefixtures('db')
 def test_acct_verif_token_returned_if_email_provided_to_function():
-    form = Bag(username=user_name, password='0123456789',
-               email=email_addr,
-               day='12', month='12', year=arrow.utcnow().year - 19)
     d.engine.execute(d.meta.tables["logincreate"].insert(), {
         "token": token,
-        "username": form.username,
-        "login_name": form.username,
+        "username": user_name,
+        "login_name": user_name,
         "hashpass": login.passhash(raw_password),
-        "email": form.email,
+        "email": email_addr,
         "birthday": arrow.Arrow(2000, 1, 1),
     })
-    acct_verification_token = login.get_account_verification_token(email=form.email, username=None)
+    acct_verification_token = login.get_account_verification_token(email=email_addr, username=None)
     assert token == acct_verification_token
 
 
 @pytest.mark.usefixtures('db')
 def test_acct_verif_token_returned_if_username_provided_to_function():
-    form = Bag(username=user_name, password='0123456789',
-               email=email_addr,
-               day='12', month='12', year=arrow.utcnow().year - 19)
     d.engine.execute(d.meta.tables["logincreate"].insert(), {
         "token": token,
-        "username": form.username,
-        "login_name": form.username,
+        "username": user_name,
+        "login_name": user_name,
         "hashpass": login.passhash(raw_password),
-        "email": form.email,
+        "email": email_addr,
         "birthday": arrow.Arrow(2000, 1, 1),
     })
-    acct_verification_token = login.get_account_verification_token(email=None, username=form.username)
+    acct_verification_token = login.get_account_verification_token(email=None, username=user_name)
     assert token == acct_verification_token

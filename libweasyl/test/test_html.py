5aec7c25d64852b71ad206f16d2bdd06c7163600
Charmander <~@charmander.me>
2024-05-14 12:30:58 -0700

    Add basic test for `strip_html`

--- libweasyl/test/test_html.py
+++ libweasyl/test/test_html.py
@@ -1,6 +1,10 @@
 from libweasyl import html
 
 
+def test_strip_html():
+    assert html.strip_html('<div style="text-align: center">**foo**</div> <!-- comment -->') == "**foo** ", "`strip_html` should strip HTML tags"
+
+
 def test_inline_json():
     assert html.inline_json('</script>') == r'"<\/script>"'
     assert html.inline_json('</SCRIPT>') == r'"<\/SCRIPT>"'

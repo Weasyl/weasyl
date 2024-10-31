f97a3bfddba4d22636a090f21d0cbf4d609df780
Hunter Turcin <huntertur@gmail.com>
2024-10-31 00:16:02 -0500

    Fix error when searching for users with symbols (#1450)
    Fixes #416
    
    Reviewer note: Known and acceptable behavior: trailing backslashes will match strings ending with `%`, which are impossible in these fields.
    
    Co-authored-by: Charmander <~@charmander.me>

--- weasyl/moderation.py
+++ weasyl/moderation.py
@@ -306,7 +306,7 @@ def get_suspension_message(userid):
     ) % (suspension.reason, release.date(), MACRO_SUPPORT_ADDRESS)
 
 
-def finduser(targetid, username, email, dateafter, datebefore, excludesuspended, excludebanned, excludeactive, ipaddr,
+def finduser(targetid, username: str, email: str, dateafter, datebefore, excludesuspended, excludebanned, excludeactive, ipaddr,
              row_offset):
     targetid = d.get_int(targetid)
 
@@ -364,12 +364,9 @@ def finduser(targetid, username, email, dateafter, datebefore, excludesuspended,
     if targetid:
         q = q.where(lo.c.userid == targetid)
     elif username:
-        q = q.where(lo.c.login_name.op('~')(username))
+        q = q.where(lo.c.login_name.ilike(f"%{username}%"))
     elif email:
-        q = q.where(sa.or_(
-            lo.c.email.op('~')(email),
-            lo.c.email.op('ilike')('%%%s%%' % email),
-        ))
+        q = q.where(lo.c.email.ilike(f"%{email}%"))
 
     # Filter for banned and/or suspended accounts
     if excludeactive == "on":

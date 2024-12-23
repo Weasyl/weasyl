633895c7ec905f203e1be3e4f60a55f371f6f4e4
Hunter Turcin <huntertur@gmail.com>
2024-12-22 18:12:59 -0600

    Warn when putting possible URL in site name field

--- assets/js/edit-profile.js
+++ assets/js/edit-profile.js
@@ -1,8 +1,28 @@
 let newSocialGroup = document.getElementById('new-social-group');
 
+function checkSuspiciousSiteName() {
+    for (const prefix of ['http://', 'https://', 'mailto:']) {
+        if (this.value.startsWith(prefix)) {
+            // Only give a warning if one of these prefixes is detected, in
+            // case a site exists in the future whose name literally starts
+            // with a URL scheme. Given how many sites exist whose names end
+            // with a top-level domain (currently .io, previously .com), better
+            // to keep this optional just in case borrowing the beginning of
+            // a URL comes into style in the future.
+            this.setCustomValidity('Did you mean to place this in the URL field?');
+            this.reportValidity();
+
+            return;
+        }
+    }
+
+    this.setCustomValidity('');
+}
+
 function markEntryForRemoval() {
-    this.parentNode.parentNode.querySelectorAll('input')
-        .forEach(element => element.disabled = true);
+    this.parentNode.parentNode.querySelectorAll('input').forEach(element => {
+        element.disabled = true;
+    });
 
     this.textContent = '🔄';
     this.className.replace('negative', 'positive');
@@ -11,8 +31,9 @@ function markEntryForRemoval() {
 }
 
 function unmarkEntryForRemoval() {
-    this.parentNode.parentNode.querySelectorAll('input')
-        .forEach(element => element.disabled = false);
+    this.parentNode.parentNode.querySelectorAll('input').forEach(element => {
+        element.disabled = false;
+    });
 
     this.textContent = '🗑';
     this.className.replace('positive', 'negative');
@@ -23,8 +44,18 @@ function unmarkEntryForRemoval() {
 function addNewSocialGroupIfNeeded() {
     if (this.querySelectorAll('input[value]')) {
         newSocialGroup = this.cloneNode(true);
-        newSocialGroup.querySelectorAll('input').forEach(element => element.value = '');
-        this.querySelector('.remove-contact').hidden = false;
+
+        newSocialGroup.querySelectorAll('input').forEach(element => {
+            element.value = '';
+        });
+
+        newSocialGroup.querySelector('input[name="site_names"]')
+            .addEventListener('input', checkSuspiciousSiteName);
+
+        const removeContact = this.querySelector('.remove-contact');
+        removeContact.hidden = false;
+        removeContact.firstChild.addEventListener('click', markEntryForRemoval);
+
         this.removeAttribute('id');
         this.insertAdjacentElement('afterend', newSocialGroup);
         this.removeEventListener('input', addNewSocialGroupIfNeeded);
@@ -34,9 +65,11 @@ function addNewSocialGroupIfNeeded() {
 
 newSocialGroup.addEventListener('input', addNewSocialGroupIfNeeded);
 
-document
-    .querySelectorAll('tr:not(#new-social-group) .remove-contact')
-    .forEach(element => {
-        element.hidden = false;
-        element.firstChild.addEventListener('click', markEntryForRemoval);
-    });
+document.querySelectorAll('tr:not(#new-social-group) .remove-contact').forEach(element => {
+    element.hidden = false;
+    element.firstChild.addEventListener('click', markEntryForRemoval);
+});
+
+document.querySelectorAll('input[name="site_names"]').forEach(element => {
+    element.addEventListener('input', checkSuspiciousSiteName);
+});

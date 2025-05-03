58a19b59bb6d4bf3f13b830a8f2682dd79a6ab10
Hunter Turcin <huntertur@gmail.com>
2025-05-03 15:37:08 -0500

    Add support for Bluesky videos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 import defineDateGroupHeader from './date-group-header.js';
+import { loadBlueskyVideo } from './embed-video.js';
 import defineLocalTime from './local-time.js';
 
 defineDateGroupHeader();
 defineLocalTime();
+loadBlueskyVideo();
--- weasyl/embed.py
+++ weasyl/embed.py
@@ -1,3 +1,5 @@
+from html import escape
+from html.parser import HTMLParser
 import re
 import string
 from urllib.parse import quote as urlquote, urlsplit
@@ -15,7 +17,21 @@ _OEMBED_MAP = {
     "vimeo": "https://vimeo.com/api/oembed.json?url=%s&maxwidth=400&maxheight=300",
     "soundcloud": "https://soundcloud.com/oembed?format=json&url=%s",
     "sketchfab": "https://sketchfab.com/oembed?url=%s&maxwidth=640&maxheight=480",
+    "bluesky": "https://embed.bsky.app/oembed?url=%s",
 }
+
+
+class BlueskyAtUriExtractor(HTMLParser):
+    """Just enough of a parser to extract the at-uri from a Bluesky oEmbed"""
+    def __init__(self):
+        self.at_uri = None
+        super().__init__()
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "blockquote":
+            for key, value in attrs:
+                if key == "data-bluesky-uri":
+                    self.at_uri = value
 
 
 def _service(link):
@@ -35,6 +51,8 @@ def _service(link):
         return "soundcloud"
     elif domain.endswith(".sketchfab.com"):
         return "sketchfab"
+    elif domain.endswith(".bsky.app"):
+        return "bluesky"
 
 
 def _targetid(link):
@@ -74,24 +92,61 @@ def _embed_json(service, targetid):
     return d.http_get(_OEMBED_MAP[service] % (urlquote(targetid),)).json()
 
 
+@region.cache_on_arguments(expiration_time=60 * 60 * 24)
+@d.record_timing
+def _embed_bluesky(targetid: str) -> str:
+    """
+    Get the best possible HTML for displaying this Bluesky-hosted video.
+
+    Attempt to generate a direct video element, if possible. If this is not
+    possible, fall back to Bluesky's oEmbed output. As of writing, the oEmbed
+    output does not directly embed a video, instead providing a clickable
+    thumbnail of the video that then leads to Bluesky.
+    """
+    oembed_html = _embed_json("bluesky", targetid)["html"]
+
+    extractor = BlueskyAtUriExtractor()
+    extractor.feed(oembed_html)
+
+    if not extractor.at_uri:
+        return oembed_html
+
+    detail_uri = f"https://public.api.bsky.app/xrpc/app.bsky.feed.getPosts?uris={extractor.at_uri}"
+
+    try:
+        detail_json = d.http_get(detail_uri).json()
+        playlist_uri: str = escape(detail_json["posts"][0]["embed"]["playlist"])
+    except Exception:
+        return oembed_html
+
+    out = f'<video id="bsky-video" src="{playlist_uri}" controls></video>'
+    out += '<p id="video-error" hidden>There was an error playing the embedded media</p>'
+
+    return out
+
+
 def html(link):
     """
     Returns the HTML code to be used in a template for a given identifier.
     """
     targetid, service = _targetid(link), _service(link)
 
-    if targetid:
-        if service in _OEMBED_MAP:
-            try:
-                return _embed_json(service, targetid)["html"]
-            except (ValueError, KeyError):
-                return "There was an error retrieving the embedded media"
-        elif service == "bandcamp":
+    if not targetid:
+        return
+
+    try:
+        if service == "bandcamp":
             return (
                 '<iframe width="400" height="100" '
                 'style="position:relative;display:block;width:400px;height:100px;" '
                 'src="https://bandcamp.com/EmbeddedPlayer/v=2/%s=%s/size=venti/bgcol=F0F0F0/linkcol=4285BB/" '
                 'allowtransparency="false" frameborder="0"></iframe>' % (targetid[0], targetid[1]))
+        elif service == "bluesky":
+            return _embed_bluesky(targetid)
+        elif service in _OEMBED_MAP:
+            return _embed_json(service, targetid)["html"]
+    except (ValueError, KeyError):
+        return "There was an error retrieving the embedded media"
 
 
 def thumbnail(link):

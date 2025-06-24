e55e0c43e327b87f39318ca85d58cdcd18b5706f
Charmander <~@charmander.me>
2025-06-23 21:10:20 -0700

    cleanup: Type `slug_for`

--- libweasyl/text.py
+++ libweasyl/text.py
@@ -14,7 +14,7 @@ except ImportError:
     from html.parser import locatestarttagend
 
 
-def slug_for(title):
+def slug_for(title: str) -> str:
     title = title.replace("&", " and ")
     return "-".join(m.group(0) for m in re.finditer(r"[a-z0-9]+", title.lower()))
 

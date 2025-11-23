Weasyl/weasyl

commit c6455b4cd8c76203344d0403a1a13dca694c07c2

Add comments to site updates (#465)
This is not very nice. Sorry.

weasyl/message.py
@@ -20,7 +20,7 @@ notification_clusters = {
     4010: 8, 4015: 8,
     4016: 9,
     4020: 10, 4025: 10, 4050: 10,
-    4030: 11, 4035: 11,
+    4030: 11, 4035: 11, 4060: 11, 4065: 11,
     4040: 12, 4045: 12,
     3150: 13,
 }
@@ -581,6 +581,48 @@ def select_comments(userid):
             INNER JOIN journal jo ON jc.targetid = jo.journalid
         WHERE we.userid = %(user)s
             AND we.type = 4035
+        ORDER BY we.unixtime DESC
+    """, user=userid))
+
+    # Site update comments
+    queries.append({
+        "type": 4060,
+        "id": i.welcomeid,
+        "unixtime": i.unixtime,
+        "userid": i.otherid,
+        "username": i.username,
+        "updateid": i.referid,
+        "title": i.title,
+        "commentid": i.targetid,
+    } for i in d.engine.execute("""
+        SELECT we.welcomeid, we.unixtime, we.otherid, we.referid, we.targetid, pr.username, up.title
+        FROM welcome we
+            INNER JOIN profile pr ON we.otherid = pr.userid
+            INNER JOIN siteupdate up ON we.referid = up.updateid
+        WHERE we.userid = %(user)s
+            AND we.type = 4060
+        ORDER BY we.unixtime DESC
+    """, user=userid))
+
+    # Site update comment replies
+    queries.append({
+        "type": 4065,
+        "id": i.welcomeid,
+        "unixtime": i.unixtime,
+        "userid": i.otherid,
+        "username": i.username,
+        "updateid": i.updateid,
+        "title": i.title,
+        "replyid": i.referid,
+        "commentid": i.targetid,
+    } for i in d.engine.execute("""
+        SELECT we.welcomeid, we.unixtime, we.otherid, we.referid, we.targetid, pr.username, up.updateid, up.title
+        FROM welcome we
+            INNER JOIN profile pr ON we.otherid = pr.userid
+            INNER JOIN siteupdatecomment uc ON we.referid = uc.commentid
+            INNER JOIN siteupdate up ON uc.targetid = up.updateid
+        WHERE we.userid = %(user)s
+            AND we.type = 4065
         ORDER BY we.unixtime DESC
     """, user=userid))
 

templates/message/notifications.html
@@ -13,7 +13,8 @@ $code:
     elif x in [4010, 4015]:       return [4010, 4015]
     elif x == 4016:               return [4016]
     elif x in [4020, 4025, 4050]: return [4020, 4025, 4050]
-    elif x in [4030, 4035]:       return [4030, 4035]
+    elif x in [4030, 4035, 4060, 4065]:
+      return [4030, 4035, 4060, 4065]
     elif x in [4040, 4045]:       return [4040, 4045]
     else:                         return []
 
@@ -30,7 +31,8 @@ $code:
     elif x in [4010, 4015]:       return "Shouts"
     elif x == 4016:               return "Staff Notes"
     elif x in [4020, 4025, 4050]: return "Submission Comments"
-    elif x in [4030, 4035]:       return "Journal Comments"
+    elif x in [4030, 4035, 4060, 4065]:
+      return "Journal Comments"
     elif x in [4040, 4045]:       return "Character Comments"
     else:                         return "Miscellaneous"
 
@@ -83,7 +85,7 @@ $:{RENDER("common/stage_title.html", ["Notifications", "Messages", 0])}
               if 'username' in i:
                 item_user = '<a href="/~' + LOGIN(i['username']) + '" class="username">' + escape_(i['username']) + '</a>'
 
-              if 'submitid' in i or 'journalid' in i or 'charid' in i:
+              if 'submitid' in i or 'journalid' in i or 'charid' in i or 'updateid' in i:
                 if 'submitid' in i:
                   folder = 'submission'
                   id = i['submitid']
@@ -93,8 +95,12 @@ $:{RENDER("common/stage_title.html", ["Notifications", "Messages", 0])}
                 elif 'charid' in i:
                   folder = 'character'
                   id = i['charid']
+                elif 'updateid' in i:
+                  folder = 'site-updates'
+                  id = i['updateid']
                 if 'title' in i:
-                  item_base_url = '/%s/%i/%s' % (folder, id, SLUG(i['title']))
+                  slug = '' if 'updateid' in i else '/%s' % (SLUG(i['title']),)
+                  item_base_url = '/%s/%i%s' % (folder, id, slug)
                   item_link = '<a href="' + item_base_url + '">' + escape_(i['title']) + '</a>'
                   if 'commentid' in i:
                     comment_url = lambda id: item_base_url + '#cid' + STR(id)
@@ -185,6 +191,12 @@ $:{RENDER("common/stage_title.html", ["Notifications", "Messages", 0])}
             $elif i['type'] == 4050:
               $# comment on collected item
               $:{item_user} left a $:{item_comment} on your collected submission $:{item_link}.
+            $elif i['type'] == 4060:
+              $# site update comment
+              $:{item_user} left a $:{item_comment} on your site update $:{item_link}.
+            $elif i['type'] == 4065:
+              $# site update comment reply
+              $:{item_user} left a $:{item_reply} to $:{item_your_comment} on the site update $:{item_link}.
             $else:
               ${i}
             <span class="date color-lighter">${DATE(i['unixtime'])} <em>at</em> ${TIME(i['unixtime'])}</span>

assets/scss/site.scss
@@ -3224,6 +3224,12 @@ img + #detail-art-text {
     clear: both;
 }
 
+#site-update-comments {
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 66em;
+}
+
 #detail-report {
     padding-bottom: 3em;
 }

libweasyl/libweasyl/alembic/versions/9270baf773a5_add_comments_to_site_updates.py
@@ -1,0 +1,42 @@
+"""Add comments to site updates
+
+Revision ID: 9270baf773a5
+Revises: 7f0e262d6370
+Create Date: 2019-01-21 16:41:17.360120
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '9270baf773a5'
+down_revision = '7f0e262d6370'
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+def upgrade():
+    # ### commands auto generated by Alembic - please adjust! ###
+    op.create_table('siteupdatecomment',
+    sa.Column('commentid', sa.Integer(), nullable=False),
+    sa.Column('userid', sa.Integer(), nullable=False),
+    sa.Column('targetid', sa.Integer(), nullable=False),
+    sa.Column('parentid', sa.Integer(), nullable=True),
+    sa.Column('content', sa.String(length=10000), nullable=False),
+    sa.Column('created_at', postgresql.TIMESTAMP(timezone=True), server_default=sa.text(u'now()'), nullable=False),
+    sa.Column('hidden_at', postgresql.TIMESTAMP(timezone=True), nullable=True),
+    sa.Column('hidden_by', sa.Integer(), nullable=True),
+    sa.CheckConstraint(u'hidden_by IS NULL OR hidden_at IS NOT NULL', name='siteupdatecomment_hidden_check'),
+    sa.ForeignKeyConstraint(['hidden_by'], ['login.userid'], name='siteupdatecomment_hidden_by_fkey', ondelete='SET NULL'),
+    sa.ForeignKeyConstraint(['targetid', 'parentid'], ['siteupdatecomment.targetid', 'siteupdatecomment.commentid'], name='siteupdatecomment_parentid_fkey'),
+    sa.ForeignKeyConstraint(['targetid'], ['siteupdate.updateid'], name='siteupdatecomment_targetid_fkey'),
+    sa.ForeignKeyConstraint(['userid'], ['login.userid'], name='siteupdatecomment_userid_fkey'),
+    sa.PrimaryKeyConstraint('commentid'),
+    sa.UniqueConstraint('targetid', 'commentid')
+    )
+    # ### end Alembic commands ###
+
+
+def downgrade():
+    # ### commands auto generated by Alembic - please adjust! ###
+    op.drop_table('siteupdatecomment')
+    # ### end Alembic commands ###

libweasyl/libweasyl/models/tables.py
@@ -1,5 +1,5 @@
 from sqlalchemy import (
-    MetaData, Table, Column, CheckConstraint, ForeignKeyConstraint, Index,
+    MetaData, Table, Column, CheckConstraint, ForeignKeyConstraint, UniqueConstraint, Index,
     Integer, String, Text, SMALLINT, text, DateTime, func, Boolean)
 from sqlalchemy.dialects.postgresql import ARRAY, JSONB, TIMESTAMP
 
@@ -729,6 +729,28 @@ siteupdate = Table(
     Column('content', Text(), nullable=False),
     Column('unixtime', WeasylTimestampColumn(), nullable=False),
     default_fkey(['userid'], ['login.userid'], name='siteupdate_userid_fkey'),
+)
+
+
+siteupdatecomment = Table(
+    'siteupdatecomment', metadata,
+    Column('commentid', Integer(), primary_key=True, nullable=False),
+    Column('userid', Integer(), nullable=False),
+    Column('targetid', Integer(), nullable=False),
+    Column('parentid', Integer(), nullable=True),
+    Column('content', String(length=10000), nullable=False),
+    Column('created_at', TIMESTAMP(timezone=True), nullable=False, server_default=func.now()),
+    Column('hidden_at', TIMESTAMP(timezone=True), nullable=True),
+    Column('hidden_by', Integer(), nullable=True),
+    ForeignKeyConstraint(['targetid'], ['siteupdate.updateid'], name='siteupdatecomment_targetid_fkey'),
+    ForeignKeyConstraint(
+        ['targetid', 'parentid'],
+        ['siteupdatecomment.targetid', 'siteupdatecomment.commentid'],
+        name='siteupdatecomment_parentid_fkey'),
+    ForeignKeyConstraint(['userid'], ['login.userid'], name='siteupdatecomment_userid_fkey'),
+    ForeignKeyConstraint(['hidden_by'], ['login.userid'], name='siteupdatecomment_hidden_by_fkey', ondelete='SET NULL'),
+    CheckConstraint("hidden_by IS NULL OR hidden_at IS NOT NULL", name='siteupdatecomment_hidden_check'),
+    UniqueConstraint('targetid', 'commentid'),
 )
 
 

templates/common/comment_form.html
@@ -9,6 +9,8 @@ $code:
     feature = "charid"
   elif feature == "journal":
     feature = "journalid"
+  elif feature == "siteupdate":
+    feature = "updateid"
 
 <form class="comment-form" action="/submit/${'shout' if feature in ['userid', 'staffnotes'] else 'comment'}" method="post">
   $:{CSRF()}

templates/etc/index.html
@@ -97,6 +97,10 @@ $ _GRID_ITEM = COMPILE("common/thumbnail_grid_item.html")
       <div class="formatted-content">
         $:{MARKDOWN(update['content'])}
       </div>
+
+      $ comment_count = update.get('comment_count')
+      $if comment_count is not None:
+        <a href="/site-updates/${update['updateid']}" class="more more-block"><i>View</i> <span>This Update</span> <i>and</i> <span>${comment_count} Comment${'' if comment_count == 1 else 's'}</span></a>
     </div>
 
   <div id="hc-critique" class="clear">

templates/etc/site_update.html
@@ -1,4 +1,4 @@
-$def with (update)
+$def with (myself, update, comments)
 $ owner = update.get_display_owner()
 <div id="home-content" class="content">
   <div class="constrained">
@@ -17,5 +17,14 @@ $ owner = update.get_display_owner()
     <div class="formatted-content" style="margin-top: 1em">
       $:{MARKDOWN(update.content)}
     </div>
+  </div>
+
+  <div id="site-update-comments">
+    $if comments:
+      <div class="constrained"><h3>Comments</h3></div>
+      $:{RENDER("common/comment_thread.html", ["detail_comments", comments, "siteupdate", myself, update.updateid, update.userid])}
+    $if myself:
+      <div class="constrained"><h3>Leave a Comment</h3></div>
+      $:{RENDER("common/comment_form.html", [myself, update.updateid, "siteupdate"])}
   </div>
 </div>

commit 649ce2e7634c6e11215dd6da2ccc79c8b97c3a75

fix: Restore staff note functionality
It turns out staff note replies and deletion were always supposed to work, and staff notes weren’t always supposed to look faded out – the shout selection code was just passing the entire `settings` string to `comment.thread` where it expected a simple hidden status boolean, which happened to be equivalent for shouts (where `h` is the only setting) but broke staff notes in a way that’s hard to notice if you weren’t familiar with the previous behaviour (`s` is always truthy, so all staff notes look hidden, and the mods who can see staff notes can see hidden comments). It looks like I broke this in c6455b4cd8c76203344d0403a1a13dca694c07c2 (Add comments to site updates)?

Reverts part of 1b61378a1e36dce3c8272c70a62e4596dce43a8f (Avoid some duplication of notification-grouping logic).

This also codifies the existing notification behaviour for deleted staff note replies of not deleting the reply notification, which I don’t think was originally intended but isn’t worth fixing.

weasyl/message.py
@@ -21,6 +21,7 @@ _NOTIFICATION_CLUSTERS: Sequence[tuple[list[int], str, str]] = [
     ([3085], "Friend Confirmations", "friend_confirmations"),
     ([3140], "Submission Tag Changes", "submission_tag_changes"),
     ([4010, 4015], "Shouts", "shouts"),
+    ([4016], "Staff Notes", "staff_notes"),
     ([4020, 4025, 4050], "Submission Comments", "submission_comments"),
     ([4030, 4035, 4060, 4065], "Journal Comments", "journal_comments"),
     ([4040, 4045], "Character Comments", "character_comments"),
@@ -468,6 +469,30 @@ def select_comments(userid):
             INNER JOIN profile px ON sh.target_user = px.userid
         WHERE we.userid = %(user)s
             AND we.type = 4015
+        ORDER BY we.unixtime DESC
+    """, user=userid))
+
+    # Staff note replies
+    queries.append({
+        "type": 4016,
+        "id": i.welcomeid,
+        "unixtime": i.unixtime,
+        "userid": i.otherid,
+        "username": i.username,
+        "ownerid": i.ownerid,
+        "ownername": i.owner_username,
+        "replyid": i.referid,
+        "commentid": i.targetid,
+    } for i in d.engine.execute("""
+        SELECT
+            we.welcomeid, we.unixtime, we.otherid, we.referid, we.targetid, pr.username, px.userid AS ownerid,
+            px.username AS owner_username
+        FROM welcome we
+            INNER JOIN profile pr ON we.otherid = pr.userid
+            INNER JOIN comments sh ON we.referid = sh.commentid
+            INNER JOIN profile px ON sh.target_user = px.userid
+        WHERE we.userid = %(user)s
+            AND we.type = 4016
         ORDER BY we.unixtime DESC
     """, user=userid))
 

weasyl/shout.py
@@ -17,7 +17,7 @@ def select(userid, ownerid, limit=None, staffnotes=False):
     statement = ["""
         SELECT
             sh.commentid, sh.parentid, sh.userid, pr.username,
-            sh.content, sh.unixtime, sh.settings, sh.hidden_by
+            sh.content, sh.unixtime, sh.settings ~ 'h', sh.hidden_by
         FROM comments sh
             INNER JOIN profile pr USING (userid)
         WHERE sh.target_user = %i
@@ -118,7 +118,7 @@ def remove(userid, *, commentid):
         id=commentid,
     ).first()
 
-    if not query or ('s' in query[2] and userid not in staff.MODS):
+    if not query or ((is_staff_note := 's' in query[2]) and userid not in staff.MODS):
         raise WeasylError("shoutRecordMissing")
 
     if userid != query[1] and userid not in staff.MODS:
@@ -132,8 +132,9 @@ def remove(userid, *, commentid):
             # a commenter cannot remove their comment if it has replies
             raise WeasylError("InsufficientPermissions")
 
-    # remove notifications
-    welcome.comment_remove(commentid, 'shout')
+    # Remove notifications. Top-level staff notes don't create notifications. For simplicity, staff note replies aren't removed; mods can see the removed comment anyway.
+    if not is_staff_note:
+        welcome.comment_remove(commentid, 'shout')
 
     # hide comment
     d.execute("UPDATE comments SET settings = settings || 'h', hidden_by = %i WHERE commentid = %i AND settings !~ 'h'", [userid, commentid])

weasyl/templates/message/notifications.html
@@ -116,6 +116,9 @@ $:{RENDER("common/stage_title.html", ["Notifications", "Messages", 0])}
                     $elif i['type'] == 4015:
                       $# shout reply
                       $:{item_user} left a $:{shout_reply('shouts')} to $:{your_comment('shouts')} on $:{item_ownername}'s shout page.
+                    $elif i['type'] == 4016:
+                      $# staff note reply
+                      $:{item_user} left a $:{shout_reply('staffnotes')} to $:{your_comment('staffnotes')} on $:{item_ownername}'s staff notes.
                     $elif i['type'] == 4020:
                       $# submission comment
                       $:{item_user} left a $:{item_comment} on your submission $:{item_link}.
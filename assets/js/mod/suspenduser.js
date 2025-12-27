7939b47d0bcbb29e387a57600dfd1870469aea34
Charmander <~@charmander.me>
2025-12-27 04:12:52 -0800

    Move ban templates entirely into client-side script
    Without the script, the `<select id="template_id">` doesn’t do anything anyway.

--- assets/js/mod/suspenduser.js
+++ assets/js/mod/suspenduser.js
@@ -1,15 +1,20 @@
-const banTemplates = JSON.parse(document.getElementById("ban-templates").textContent);
+import BAN_TEMPLATES from './ban-templates.json' with {type: 'json'};
+
 const templateId = document.getElementById("template_id");
 const suspendAction = document.getElementById("suspendaction");
 const suspendDuration = document.getElementById("duration");
 const suspendReason = document.getElementById("suspendreason");
+
+for (const [key, {name}] of Object.entries(BAN_TEMPLATES)) {
+  templateId.options.add(new Option(name, key));
+}
 
 function fillCustomAction() {
   if (!this.value) {
     return;
   }
 
-  const tmpl = banTemplates[this.value];
+  const tmpl = BAN_TEMPLATES[this.value];
 
   if (tmpl.days < 0) {
     suspendAction.selectedIndex = 2;
@@ -23,3 +28,4 @@ function fillCustomAction() {
 }
 
 templateId.addEventListener("change", fillCustomAction);
+templateId.disabled = false;
--- libweasyl/test/test_html.py
+++
@@ -1,7 +0,0 @@
-from libweasyl import html
-
-
-def test_inline_json():
-    assert html.inline_json('</script>') == r'"\u003c/script>"'
-    assert html.inline_json('</SCRIPT>') == r'"\u003c/SCRIPT>"'
-    assert html.inline_json('<!--<script>') == r'"\u003c!--\u003cscript>"'

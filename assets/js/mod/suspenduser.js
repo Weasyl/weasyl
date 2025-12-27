b66cef127a415838ec65604f1f95f089ebb89333
Charmander <~@charmander.me>
2025-12-27 04:18:11 -0800

    cleanup: reformat

--- assets/js/mod/suspenduser.js
+++ assets/js/mod/suspenduser.js
@@ -1,31 +1,31 @@
 import BAN_TEMPLATES from './ban-templates.json' with {type: 'json'};
 
-const templateId = document.getElementById("template_id");
-const suspendAction = document.getElementById("suspendaction");
-const suspendDuration = document.getElementById("duration");
-const suspendReason = document.getElementById("suspendreason");
+const templateId = document.getElementById('template_id');
+const suspendAction = document.getElementById('suspendaction');
+const suspendDuration = document.getElementById('duration');
+const suspendReason = document.getElementById('suspendreason');
 
 for (const [key, {name}] of Object.entries(BAN_TEMPLATES)) {
-  templateId.options.add(new Option(name, key));
+    templateId.options.add(new Option(name, key));
 }
 
 function fillCustomAction() {
-  if (!this.value) {
-    return;
-  }
+    if (!this.value) {
+        return;
+    }
 
-  const tmpl = BAN_TEMPLATES[this.value];
+    const tmpl = BAN_TEMPLATES[this.value];
 
-  if (tmpl.days < 0) {
-    suspendAction.selectedIndex = 2;
-    suspendDuration.value = "";
-  } else {
-    suspendAction.selectedIndex = 1;
-    suspendDuration.value = tmpl.days;
-  }
+    if (tmpl.days < 0) {
+        suspendAction.selectedIndex = 2;
+        suspendDuration.value = '';
+    } else {
+        suspendAction.selectedIndex = 1;
+        suspendDuration.value = tmpl.days;
+    }
 
-  suspendReason.defaultValue = tmpl.reason;
+    suspendReason.defaultValue = tmpl.reason;
 }
 
-templateId.addEventListener("change", fillCustomAction);
+templateId.addEventListener('change', fillCustomAction);
 templateId.disabled = false;

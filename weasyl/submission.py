Weasyl/weasyl

commit adcc34f7ed222870d1388be47b1a0d6971ad66ba

Prevent even mods from changing others’ submission covers
This ability isn’t exposed in the frontend, and it doesn’t really make sense – “Remove Cover Art” already exists as a mod action.

weasyl/submission.py
@@ -1101,7 +1101,7 @@ def reupload_cover(userid, submitid, coverfile):
 
     if not query:
         raise WeasylError("Unexpected")
-    elif userid not in staff.MODS and userid != query[0]:
+    elif userid != query[0]:
         raise WeasylError("InsufficientPermissions")
     elif query[1] < 2000:
         raise WeasylError("Unexpected")

commit 84b58000f9305d5aa225903a9c12096dfd9f3628

fix: mod cover removal
Broken in adcc34f7ed222870d1388be47b1a0d6971ad66ba (Prevent even mods from changing others’ submission covers).

I think I might’ve had a different fix and cleanup in progress for this, but I can’t find it right now.

weasyl/submission.py
@@ -1138,17 +1138,19 @@ def reupload_cover(userid, submitid, coverfile):
         "SELECT userid, subtype FROM submission WHERE submitid = %(id)s",
         id=submitid).first()
 
+    is_removal = not coverfile
+
     if not query:
         raise WeasylError("submissionRecordMissing")
-    elif userid != query[0]:
+    elif userid != query[0] and not (is_removal and userid in staff.MODS):
         raise WeasylError("InsufficientPermissions")
     elif query[1] < 2000:
         raise WeasylError("Unexpected")
 
-    cover_media_item = media.make_cover_media_item(coverfile)
-    if not cover_media_item:
+    if is_removal:
         orm.SubmissionMediaLink.clear_link(submitid, 'cover')
     else:
+        cover_media_item = media.make_cover_media_item(coverfile)
         orm.SubmissionMediaLink.make_or_replace_link(submitid, 'cover', cover_media_item)
 
 